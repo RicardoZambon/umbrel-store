06e7a43428618acda7c64fbf95f7f4812769ea6b
Ricardo Zambon <samuraizambon@outlook.com>
2026-03-18 08:19:05 -0300

    fix: docker build

--- zambon-niten-devenv/entrypoint-custom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start SSH server in the background
-sudo /usr/sbin/sshd
-
-# Start code-server
-exec /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8443 --auth password /home/coder/workspace
